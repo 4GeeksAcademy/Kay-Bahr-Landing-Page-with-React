1bcce78782146b265ffce20388f725d04247b1a8
KayBahr <124631379+KayBahr@users.noreply.github.com>
2023-04-21 11:26:05 +0000

    card commit 1

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -8,7 +8,8 @@ function Card(){
             <div className="card-body">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">Lorem ipsum dolor sit amet. Ut fugiat adipisci hic itaque dolore in enim molestiae aut dolores atque. Id explicabo eius in vero laborum in dolore minima.</p>
-                <a href="#" className="btn btn-primary">Find out more!</a>
+                <hr />
+                <a href="#" className="btn btn-primary" id="cardButton">Find out more!</a>
             </div>
         </div>
     );
--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -4,8 +4,8 @@ import React from "react";
 function Jumbotron(){
     return (
         <div className="jumbotron">
-            <h1 className="display-4">A Warm Welcome</h1>
-            <p className="lead">Lorem ipsum dolor sit amet. Ut fugiat adipisci hic itaque dolore in enim molestiae aut dolores atque. Id explicabo eius in vero laborum in dolore minima.</p>
+            <h1 className="display-4"><strong>A Warm Welcome!</strong></h1>
+            <p className="lead"><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</strong></p>
             <p className="lead">
                 <a className="btn btn-primary btn-lg" href="#" role="button">Call to action!</a>
             </p>
